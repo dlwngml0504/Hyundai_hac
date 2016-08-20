99aca8aa1007165b08d1442278625709ee69edec
juhee <dlwngml0504@naver.com>
2016-08-20 11:07:14 +0900

    ListView -ing

--- app/src/main/java/com/example/juhee/hyundai/MainActivity.java
+++ app/src/main/java/com/example/juhee/hyundai/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.juhee.hyundai;
 
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,17 +9,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Calendar;
+import java.util.TimeZone;
+
 
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> mTownAdapter = null;
@@ -30,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView mListview ;
+        ListViewAdapter mAdapter;
+
+        mAdapter = new ListViewAdapter() ;
+
+        mListview = (ListView) findViewById(R.id.stationlistview1);
+        mListview.setAdapter(mAdapter);
 
         final List<String> cityList = new ArrayList<String>(Arrays.asList("강원","경기","경남","경북","광주","대구","대전","부산","서울","울산","인천","전남","전북","제주","충남","충북"));
         String[] car_str=getResources().getStringArray(R.array.carSpinnerArray);
@@ -48,6 +61,11 @@ public class MainActivity extends AppCompatActivity {
         mCarSpinner.setAdapter(mCarAdapter);
         mCitySpinner.setAdapter(mCityAdapter);
         mTownSpinner.setAdapter(mTownAdapter);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        sdf.setTimeZone(TimeZone.getDefault());
+        String currentDateandTime = sdf.format(new Date());
+        Log.e("seconds~~~~~~~~~~~~~~~~", currentDateandTime);
 
         mCitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -101,14 +119,34 @@ public class MainActivity extends AppCompatActivity {
                         UrlConnection urlconn = new UrlConnection();
                         try {
                             //urlconn.GetSupply(mCitySpinner.getSelectedItem().toString(),mTownSpinner.getSelectedItem().toString(),mStationType);
-                            urlconn.GetSupply(cityList.get(mcityPosition-1),mTownSpinner.getSelectedItem().toString(),mStationType);
+                            ArrayList<String> mStation = urlconn.GetSupply(cityList.get(mcityPosition-1),mTownSpinner.getSelectedItem().toString(),mStationType);
+                            for (int i=0;i<mStation.size();i++) {
+                                JSONObject jo= new JSONObject(mStation.get(i));
+                                Log.e("******************", String.valueOf(i));
+                                Log.e("location",jo.getString("map"));
+                                Log.e("holiday",jo.getString("holiday"));
+                                Log.e("address",jo.getString("address"));
+                            }
                         } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 }).start();
             }
         });
+
+
+        // 첫 번째 아이템 추가.
+        mAdapter.addItem(ContextCompat.getDrawable(this, R.drawable.hyundai),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        mAdapter.addItem(ContextCompat.getDrawable(this, R.drawable.hyundai),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        mAdapter.addItem(ContextCompat.getDrawable(this, R.drawable.hyundai),
+                "Ind", "Assignment Ind Black 36dp") ;
     }
     private class ShowTownSpinner extends AsyncTask<String, Void, ArrayList<String>> {
 
